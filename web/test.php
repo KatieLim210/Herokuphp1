8d8a66990b8452d63e7d4e91182ebb2a8fee853e
21023701@myrp.edu.sg <21023701@myrp.edu.sg>
2023-01-05 15:15:22 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -5,6 +5,8 @@
 <!-- fix: document doesn’t have a <title> element-->
 <title>exercise 3</title>
 <head>
+  <!-- fix: Does not have a <meta name="viewport"> tag with width or initial-scale -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
